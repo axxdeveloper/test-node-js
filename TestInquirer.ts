9546cfcf1ef6e09300faa1c560d6e5d7f0ffc96f
shooeugenesea <shooeugenesea.tw@gmail.com>
2020-07-16 09:11:22 +0800

    Support add item to TODO list

--- TestInquirer.ts
+++ TestInquirer.ts
@@ -2,24 +2,49 @@ import * as inquirer from "inquirer"
 
 // Object.values(Commands) will become [List, Quit, 1, 2] without specifying string value
 enum Commands {
-    List = "List",
+    AddTodo = "AddTodo",
     Quit = "Quit"
 }
 
-inquirer.prompt({
-    type: "list",
-    name: "command",
-    message: "Choose option",
-    choices: Object.values(Commands)
-}).then(answers => {
-    switch (answers["command"]) {
-        case Commands.List:
-            console.log("Select List")
-            break;
-        case Commands.Quit:
-            console.log("Select Quit")
-            break;
-        default:
-            console.log("Missing " + answers["command"])
-    }
-})
+promptList()
+
+var todo: Array<string> = []
+
+function listTodo() {
+    console.log(todo)
+}
+
+function promptAdd() {
+    inquirer.prompt({
+        type: "input",
+        name: "add",
+        message: "Input TODO:",
+    }).then(answers => {
+        if (answers['add'] != "") {
+            todo.push(answers['add'])
+        }
+        promptList();
+    })
+}
+
+function promptList() {
+    console.clear();
+    listTodo();
+    inquirer.prompt({
+        type: "list",
+        name: "command",
+        message: "Choose option",
+        choices: Object.values(Commands)
+    }).then(answers => {
+        switch (answers["command"]) {
+            case Commands.AddTodo:
+                promptAdd();
+                break;
+            case Commands.Quit:
+                console.log("Select Quit")
+                break;
+            default:
+                console.log("Missing " + answers["command"])
+        }
+    })
+}
