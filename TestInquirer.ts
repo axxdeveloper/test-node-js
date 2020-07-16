a9bc2d47208cb0feec04e9cebea71d7fcadcb6a4
Isaac Liao <shooeugenesea.tw@gmail.com>
2020-07-16 19:21:16 +0800

    support mark todo item done

--- TestInquirer.ts
+++ TestInquirer.ts
@@ -2,17 +2,61 @@ import * as inquirer from "inquirer"
 
 // Object.values(Commands) will become [List, Quit, 1, 2] without specifying string value
 enum Commands {
-    AddTodo = "AddTodo",
+    Add = "Add",
+    Done = "Done",
     Quit = "Quit"
 }
 
+let todo: Array<Task> = []
+
 promptList()
 
-var todo: Array<string> = []
+class Task {
+    done: boolean = false;
+    constructor(public name:string) {
+    }
+    markDone() {
+        this.done = true
+    }
+    toString = ():string => {
+        return this.name + "=>" + (this.done ? "done" : "not done")
+    }
+}
+
 
 function listTodo() {
-    console.log(todo)
+    todo.forEach(item => {
+        console.log(`${item}`)
+    })
 }
+
+function promptDone() {
+    inquirer.prompt({
+        type: "checkbox",
+        name: "check",
+        message: "Mark done:",
+        choices: todo.map(item => ({
+            name: item.name,
+            value: item.name,
+            checked: item.done
+        }))
+    }).then(answers => {
+        console.log(answers)
+        let checkedItems = answers['check'] as string[]
+        if (answers['check'] != "") {
+            todo.forEach(item => item.done = false)
+            todo.forEach(item => {
+                checkedItems.forEach(checkedItem => {
+                    if (item.name == checkedItem) {
+                        item.done = true
+                    }
+                })
+            })
+        }
+        promptList();
+    })
+}
+
 
 function promptAdd() {
     inquirer.prompt({
@@ -21,7 +65,7 @@ function promptAdd() {
         message: "Input TODO:",
     }).then(answers => {
         if (answers['add'] != "") {
-            todo.push(answers['add'])
+            todo.push(new Task(answers['add']))
         }
         promptList();
     })
@@ -37,8 +81,11 @@ function promptList() {
         choices: Object.values(Commands)
     }).then(answers => {
         switch (answers["command"]) {
-            case Commands.AddTodo:
+            case Commands.Add:
                 promptAdd();
+                break;
+            case Commands.Done:
+                promptDone();
                 break;
             case Commands.Quit:
                 console.log("Select Quit")
